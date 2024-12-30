1db9d96fb616832acaee17c48495f681e8592956
Charu Chandra Joshi <charu.ccj@gmail.com>
2024-12-30 22:54:04 +0530

    completes hello

--- go_fundamentals/hello/hello_test.go
+++ go_fundamentals/hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Charu")
-	want := "Hello, Charu"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Charu")
+		want := "Hello, Charu"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("say 'Hello, World' when empty string", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
